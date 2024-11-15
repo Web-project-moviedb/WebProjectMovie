0196ce60198277f3896e9a36ab0669bbddd110fb
Sanna Tikkakoski <153513791+sannatikk@users.noreply.github.com>
2024-11-15 13:31:27 +0200

    expanded movie details, added collections

--- src/api/fetchTMDB.js
+++ src/api/fetchTMDB.js
@@ -2,35 +2,35 @@
 // to change sorting to tmdb popularity (don't ask me what it's based on, I have no idea bc the results are v weird), change the sort_by value to 'popularity.desc'
 // to change sorting to revenue (note that this does not take inflation into account), change the sort_by value to 'revenue.desc'
 
+const tmdb_token = process.env.REACT_APP_TMDB_TOKEN
+
 const fetchMovieById = async (id) => {
 
-  const tmdb_token = process.env.REACT_APP_TMDB_TOKEN;
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
       Authorization: tmdb_token
     }
-  };
+  }
 
   try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US', options`, options);
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US', options`, options)
     if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      throw new Error(`Error: ${response.status} - ${response.statusText}`)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
 
   } catch (error) {
-    console.error('Error fetching movies:', error);
-    throw error;
+    console.error('Error fetching movies:', error)
+    throw error
   }
 }
 
 
 const fetchMoviesByTerm = async (term) => {
   
-  const tmdb_token = process.env.REACT_APP_TMDB_TOKEN;
   const options = {
     method: 'GET',
     headers: {
@@ -42,100 +42,119 @@ const fetchMoviesByTerm = async (term) => {
   try {
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${term}&include_adult=false&language=en-US&page=1&sort_by=vote_count.desc`, options);
     if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      throw new Error(`Error: ${response.status} - ${response.statusText}`)
     }
-    const data = await response.json();
+    const data = await response.json()
     return data;
 
   } catch (error) {
-    console.error('Error fetching movies:', error);
-    throw error;
+    console.error('Error fetching movies:', error)
+    throw error
   }
 }
 
 
 const fetchMoviesByYear = async (year) => {
   
-  const tmdb_token = process.env.REACT_APP_TMDB_TOKEN;
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
       Authorization: tmdb_token
     }
-  };
+  }
 
   try {
     const response = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_count.desc&primary_release_year=${year}`, options);
     if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      throw new Error(`Error: ${response.status} - ${response.statusText}`)
     }
-    const data = await response.json();
-    return data; 
+    const data = await response.json()
+    return data
 
   } catch (error) {
-    console.error('Error fetching movies:', error);
-    throw error;
+    console.error('Error fetching movies:', error)
+    throw error
   }
-};
+}
 
 
 const fetchMoviesByLanguage = async (lang) => {
   
-  const tmdb_token = process.env.REACT_APP_TMDB_TOKEN;
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
       Authorization: tmdb_token
     }
-  };
+  }
 
   try {
     const response = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_count.desc&with_original_language=${lang}`, options);
     if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      throw new Error(`Error: ${response.status} - ${response.statusText}`)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
 
   } catch (error) {
-    console.error('Error fetching movies:', error);
-    throw error;
+    console.error('Error fetching movies:', error)
+    throw error
   }
-};
+}
 
 
 const fetchGenres = async () => {
 
-  const tmdb_token = process.env.REACT_APP_TMDB_TOKEN;
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
       Authorization: tmdb_token
     }
-  };
+  }
 
   try {
     const response = await fetch(`  https://api.themoviedb.org/3/genre/movie/list?language=en`, options);
     if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      throw new Error(`Error: ${response.status} - ${response.statusText}`)
     }
     const data = await response.json();
-    return data;
+    return data
 
   } catch (error) {
-    console.error('Error fetching movies:', error);
-    throw error;
+    console.error('Error fetching movies:', error)
+    throw error
   }
-
 }
 
 
 const fetchMoviesByGenre = async (genre) => {
   
-  const tmdb_token = process.env.REACT_APP_TMDB_TOKEN;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: tmdb_token
+    }
+  }
+
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_count.desc&with_genres=${genre}`, options);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} - ${response.statusText}`)
+    }
+    const data = await response.json()
+    return data
+
+  } catch (error) {
+    console.error('Error fetching movies:', error)
+    throw error
+  }
+}
+
+const fetchCollection = async (collection) => {
+  
   const options = {
     method: 'GET',
     headers: {
@@ -145,18 +164,18 @@ const fetchMoviesByGenre = async (genre) => {
   };
 
   try {
-    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_count.desc&with_genres=${genre}`, options);
+    const response = await fetch(`https://api.themoviedb.org/3/collection/${collection}?language=en-US`, options)
     if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      throw new Error(`Error: ${response.status} - ${response.statusText}`)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
 
   } catch (error) {
-    console.error('Error fetching movies:', error);
-    throw error;
+    console.error('Error fetching movies:', error)
+    throw error
   }
 }
 
 
-export { fetchMovieById, fetchMoviesByTerm, fetchMoviesByYear, fetchMoviesByLanguage, fetchMoviesByGenre, fetchGenres }
+export { fetchMovieById, fetchMoviesByTerm, fetchMoviesByYear, fetchMoviesByLanguage, fetchMoviesByGenre, fetchGenres, fetchCollection }
