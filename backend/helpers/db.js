357a4bba85426563f245eb6f244d913f0a58c13b
Sanna Tikkakoski <153513791+sannatikk@users.noreply.github.com>
2024-11-26 09:48:58 +0200

    update packages and structure

--- backend/helpers/db.js
+++ backend/helpers/db.js
@@ -6,10 +6,17 @@ dotenv.config()
 const { Pool } = pkg
 
 const openDb = () => {
+
+    const environment = process.env.NODE_ENV // get current environment
+    console.log("Current environment: ", environment)
+
+    const dbName = environment === 'development' ? process.env.DB_NAME : process.env.TEST_DB_NAME // select database based on environment
+    console.log("Connecting to database: ", dbName)
+
     const pool = new Pool({
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
-        database: process.env.DB_NAME, // This can be modified to add logic to select development or test database
+        database: dbName,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT
     })
--- backend/jest.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    testEnvironment: "node",
-    transform: {},
-    roots: ["<rootDir>/tests"], // Jest will look for tests only in the 'tests' folder
-  };
