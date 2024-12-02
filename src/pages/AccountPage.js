1b83cc59e3937e7b1dc4a4bfef845e0b619e8d1d
Sanna Tikkakoski <153513791+sannatikk@users.noreply.github.com>
2024-12-02 14:41:30 +0200

    minor fixes

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import { useParams, Link, useNavigate } from 'react-router-dom'
 import { UseUser } from '../context/UseUser.js'
 import ReviewsByUser from '../components/reviews/ReviewsByUser.js'
-import { MainHeader, SectionHeader } from "../components/Header.js"
+import { MainHeader, SectionHeader } from "../components/header/Header.js"
 import ProfileFavoriteList from "../components/account/AccountFavorites.js"
 import ProfileGroupList from "../components/account/AccountGroups.js"
 
