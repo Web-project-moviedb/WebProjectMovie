c2047db7fb524220592388ebfc9fd70bd456ac9d
Sanna Tikkakoski <153513791+sannatikk@users.noreply.github.com>
2024-12-04 15:05:58 +0200

    groups styling

--- src/components/groups/CreateGroupForm.js
+++ src/components/groups/CreateGroupForm.js
@@ -1,37 +1,54 @@
 import React from "react"
 import { UseUser } from "../../context/UseUser.js"
 
-
 export default function CreateGroupForm({ onCreateGroup }) {
     const { user } = UseUser()
     const [groupName, setGroupName] = React.useState('')
     const [groupDescription, setGroupDescription] = React.useState('')
+    const [error, setError] = React.useState(null)
 
     if (!user.id) {
-        return <div>You must be logged in to create a group</div>
+        return <div><i>You must be logged in to create a group.</i></div>
     }
+
     const handleCreateGroup = (e) => {
         e.preventDefault()
         if (groupName === '' || groupDescription === '') {
-            alert('Group name and description is required')
+            setError('Group name and description are required.')
             return
         }
         onCreateGroup(groupName, groupDescription, user.id)
         setGroupName('')
         setGroupDescription('')
+        setError(null)
     }
 
     return (
-        <form onSubmit={handleCreateGroup}>
-            <label>
-                Group Name:
-                <input type='text' value={groupName} onChange={(e) => setGroupName(e.target.value)} />
-            </label>
-            <label>
-                Description:
-                <textarea value={groupDescription} onChange={(e) => setGroupDescription(e.target.value)} />
-            </label>
-            <button type='submit'>Create Group</button>
-        </form>
+        <div>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <form className="create-group-form" onSubmit={handleCreateGroup}>
+                <div className="group-fields">
+                    <div className="group-name-field">
+                        <label htmlFor="group_name">Group Name:</label>
+                        <textarea
+                            id="group_name"
+                            value={groupName}
+                            onChange={(e) => setGroupName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="group-description-field">
+                        <label htmlFor="group_description">Group Description:</label>
+                        <textarea
+                            id="group_description"
+                            value={groupDescription}
+                            onChange={(e) => setGroupDescription(e.target.value)}
+                            required
+                        />
+                    </div>
+                </div>
+                <button type="submit">Create Group</button>
+            </form>
+        </div>
     )
 }
--- src/pages/Groups.js
+++ src/pages/Groups.js
@@ -3,6 +3,7 @@ import AllGroups from '../components/groups/AllGroups.js'
 import { MainHeader, SectionHeader } from '../components/header/Header.js'
 import { createGroup, fetchAllGroups } from "../utils/groupFunctions.js"
 import CreateGroupForm from "../components/groups/CreateGroupForm.js"
+import './Groups.css'
 
 /* All users can view groups page
 Groups page contains all the created groups
@@ -43,7 +44,7 @@ export default function Groups() {
         <div>
             <MainHeader text={'Groups'} />
             <AllGroups groups={groups} />
-            <SectionHeader text={'Create Group'} />
+            <SectionHeader text={'Create New Group'} />
             <CreateGroupForm onCreateGroup={handleCreateGroup} />
         </div>
     )
